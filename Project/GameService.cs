7bb6ebbe428f6d2b48aaa9fe029b5e6a5baa5913
Brett Wilcox <wilcox.brett@gmail.com>
2019-04-04 19:55:17 -0600

    Everything working!

--- Project/GameService.cs
+++ Project/GameService.cs
@@ -12,6 +12,7 @@ namespace CastleGrimtol.Project
     public Player CurrentPlayer { get; set; }
     public bool Playing { get; set; }
     public int count { get; set; } = 0;
+    public bool StoppedFlooding { get; set; } = false;
 
     //Start the game
     public void Run()
@@ -21,7 +22,7 @@ namespace CastleGrimtol.Project
       Intro();
       while (Playing)
       {
-        if (count >= 11)
+        if (count >= 12)
         {
           Console.Clear();
           Console.WriteLine("You wasted too much time. Your boat is underwater. Abandon ship and swim to shore. \nYou lose. Better luck next time. \n \nDo you want to play again?");
@@ -124,7 +125,7 @@ namespace CastleGrimtol.Project
     public void SetUp()
     {
       Room aft = new Room("aft cabin", "You're in the aft cabin, this is where you were sleeping. The only thing here is your bed, clothes, and personal items. There's a small window to port, but too small for you to squeeze out of. The only door leads forward.");
-      Room passageway = new Room("passageway", "You're in the passageway, which runs forward and aft. There's water on the deck and the water level is rising quickly. \nFrom the passageway you can enter the aft cabin, forward berth, port side, or starboard side of the boat.");
+      Room passageway = new Room("passageway", "You're in the passageway, which runs forward and aft. There's water on the deck and the water level is rising quickly. \nFrom the passageway you can enter the aft cabin, forward berth, port side, starboard side, \nor go up to the cockpit outside.");
       Room forward = new Room("forward berth", "As you enter the forward berth you feel a rush of water on your feet and legs and you see a large hole the size of a \nsoccerball on the forward-most starboard side, water is rushing into your boat.");
       Room starboard = new Room("starboard", "The starboard side of the boat has a Coast Guard radio and a galley with food and dishes. \nYou're feeling hungry, maybe you should make a sandwich, afterall the water is only up to your ankles.");
       Room port = new Room("port", "The port side of the boat has nautical gear scattered about, several piles of books and charts, and a \nsoccerball that never gets used (because you live on a boat).");
@@ -158,7 +159,7 @@ namespace CastleGrimtol.Project
     {
       Console.Clear();
       Console.WriteLine("Your goal is to stop the water from flooding your boat, then make a mayday call for help. \nHurry, if you waste too much time your boat will sink and you'll lose the game.");
-      Console.WriteLine("\nIf you're not familiar with boats you can move: \nforward to go toward the front of the boat, \naft to go toward the back of the boat, \nport to go to the left side of the boat, and \nstarboard to go to the right side of the boat.");
+      Console.WriteLine("\nIf you're not familiar with boats you can move: \nforward to go toward the front of the boat, \naft to go toward the back of the boat, \nport to go to the left side of the boat, \nstarboard to go to the right side of the boat, \nup to go outside to the cockpit of the boat, and \ndown to go back down into the boat.");
       Console.WriteLine("\nPress any key to begin. Good luck.");
       Console.ReadKey();
       Console.Clear();
@@ -255,7 +256,7 @@ namespace CastleGrimtol.Project
       Console.Clear();
       Console.WriteLine(@"When prompted you can respond with following commands:
 
-Go + a direction (ex. go forward, aft, port, or starboard),
+Go + a direction (ex. go forward, aft, port, starboard, up, or down),
 Use + the item name (ex. use screwdriver), 
 Take + the item name (ex. take peanut butter), 
 Look (to get a description of the room), 
@@ -273,9 +274,16 @@ Press any key to continue.");
     public void Inventory()
     {
       Console.Clear();
-      for (int i = 0; i < CurrentPlayer.Inventory.Count; i++)
+      if (CurrentPlayer.Inventory.Count > 0)
       {
-        Console.WriteLine(value: $"{i + 1}. {CurrentPlayer.Inventory[i].Name.ToUpper()[0] + CurrentPlayer.Inventory[i].Name.Substring(1)}: {CurrentPlayer.Inventory[i].Description}");
+        for (int i = 0; i < CurrentPlayer.Inventory.Count; i++)
+        {
+          Console.WriteLine(value: $"{i + 1}. {CurrentPlayer.Inventory[i].Name.ToUpper()[0] + CurrentPlayer.Inventory[i].Name.Substring(1)}: {CurrentPlayer.Inventory[i].Description}");
+        }
+      }
+      else
+      {
+        Console.WriteLine("You don't have any items in your inventory. Go find some. \n");
       }
       Console.WriteLine("\nWhat would you like to do?");
       string response = Console.ReadLine().ToLower();
@@ -349,12 +357,72 @@ Press any key to continue.");
       {
         return i.Name.ToLower() == name;
       });
-      if (CurrentRoom.Description[0] == 'A' && CurrentPlayer.Inventory.Count > 0)
+      if (CurrentRoom.Description[0] == 'A' && name == "soccerball")
       {
         CurrentPlayer.Inventory.Remove(item);
+        StoppedFlooding = true;
         CurrentRoom.Description = "You're in the forward berth, it's a mess and there's water everywhere but the soccerball is plugging the hole \nand there's no water leaking in.";
         Console.Clear();
-        Console.WriteLine("Congratulations! You saved your boat from sinking. It's a good thing you had that soccerball. \nNow hurry and go make a mayday call. \n ");
+        Console.WriteLine("Congratulations! You saved your boat from sinking. It's a good thing you had that soccerball. \nNow hurry and go up to the cockpit outside to make a mayday call. \n ");
+      }
+      else if (CurrentRoom.Description[7] == 'o' && name == "radio" && StoppedFlooding)
+      {
+        CurrentPlayer.Inventory.Remove(item);
+        Console.Clear();
+        Console.WriteLine("Great, you're trying to use the radio to make a mayday call, but the radio is on the wrong frequency. \nTo go to the correct frequency you first have to answer a complicated radio question. \nBe careful, if you answer incorrectly the radio will short circut and you'll be stranded with no help on the way, \nwhich means you will lose the game. \n");
+        Console.WriteLine(@"Does the following return true or false?
+
+  Given two strings does str2 contain all of the letters of str1 in the order they occur in str1?
+  let str1 = 'cat'
+  let str2 = 'rclnaztsw'
+
+  function sameCharacters(str1, str2) {
+  let count = 0
+  let length = str1.length
+  for (let i = 0; i < str2.length; i++) {
+    if (str2[i] == str1[count]){
+      count++
+      if (count == length){
+        return true
+      }
+    }
+  }
+  return false
+  }");
+        Console.WriteLine("\n");
+        string answer = Console.ReadLine().ToLower();
+        if (answer == "true")
+        {
+          Console.Clear();
+          Console.WriteLine("Congratulations you won the game! \nYou answered correctly and your mayday call will now go through. Help is on the way.\nWell done captain, you're a worthy sailor. Cheers. \nWant to play again? Yes/No");
+          string play = Console.ReadLine().ToLower();
+          if (play[0] == 'y')
+          {
+            Reset();
+          }
+          else
+          {
+            Console.Clear();
+            Console.WriteLine("Goodbye.");
+            Playing = false;
+          }
+        }
+        else
+        {
+          Console.Clear();
+          Console.WriteLine("Oh no! You answered incorrectly. \nThe radio has short-circuted and you have no way to call for help (except maybe your computer and cellphone dut disregard those items). \n \nYou lose. Game over. \nStudy up buttercup, better luck next time. \nWant to play again? Yes/No");
+          string play = Console.ReadLine().ToLower();
+          if (play[0] == 'y')
+          {
+            Reset();
+          }
+          else
+          {
+            Console.Clear();
+            Console.WriteLine("Goodbye.");
+            Playing = false;
+          }
+        }
       }
       else
       {
